02730838cda47e7f8de388d1cc53e01577fcb548
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2018-12-22 03:01:06 +0900

    Add base files for calculator and its tests

---
+++ calculator.py
@@ -0,0 +1,16 @@
+"""Simple calculator example for the practice of collaboration."""
+
+
+class Calculator(object):
+    """Simple calculator which has 4 functionalities.
+
+    => add, sub, mul, div
+
+    All methods will be implemented as staticmethods
+    so that it can be called without making an object.
+
+    !!Don't forget to run static analyzers and unittests
+    before pushing your code. (check 100% test covered)
+    """
+
+    pass
---
+++ test_calculator.py
@@ -0,0 +1,28 @@
+"""Test cases for calculator.py."""
+
+# from calculator import Calculator
+
+
+class TestCalculator():
+    """This class has 4 methods to test add, sub, mul, and div respectively.
+
+    Note that the test class doesn't have a constructor.
+
+    !!Don't forget to check all implementation covered by tests.
+    """
+
+    def test_add(self):
+        """Put your test cases for add that starts with assert."""
+        pass
+
+    def test_sub(self):
+        """Put your test cases for sub that starts with assert."""
+        pass
+
+    def test_mul(self):
+        """Put your test cases for mul that starts with assert."""
+        pass
+
+    def test_div(self):
+        """Put your test cases for div that starts with assert."""
+        pass

4119ab0cd9e181671f5803346cefd4790f2b8057
Seo Yeon Stella Yang <howtowhy@gmail.com>
2019-01-12 02:27:34 +0900

    Origin/user/howtowhy/mul (#19)
    * Define Mul
    
    * Change type definition
    
    * Change type definition
    
    * Add division to Calculator class (#10) (#12)
    
    This is division operation for Calculator class.
    
    * Change Style
    
    * Correct the flake8 errors

--- calculator.py
+++ calculator.py
@@ -1,8 +1,36 @@
 """Simple calculator example for the practice of collaboration."""
+# -*- coding: utf-8 -*-
+"""
+    Examples can be given using either the ``Example`` or ``Examples``
+    sections. Sections support any reStructuredText formatting, including
+    literal blocks::
+
+        $ python example_google.py
+
+Section breaks are created by resuming unindented text. Section breaks
+are also implicitly created anytime a new section starts.
+
+Attributes:
+    module_level_variable1 (int): Module level variables may be documented in
+        either the ``Attributes`` section of the module docstring, or in an
+        inline docstring immediately following the variable.
+
+        Either form is acceptable, but the two should not be mixed. Choose
+        one convention to document module level variables and be consistent
+        with it.
+
+Todo:
+    * validate the code
+
+.. _Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+
+"""
 
 
 class Calculator(object):
-    """Simple calculator which has 4 functionalities.
+    """
+    Simple calculator which has 4 functionalities.
 
     => add, sub, mul, div
 
@@ -12,5 +40,34 @@ class Calculator(object):
     !!Don't forget to run static analyzers and unittests
     before pushing your code. (check 100% test covered)
     """
-    def mul(self, a, b):
-        return a*b
+
+    def mul(sef, a: float, b: float) -> float:
+        """Multiply the inputs.
+
+        This fuction is multiply the float two inputs:
+
+        Args:
+            a (float): The first parameter.
+            b (float): The second parameter.
+
+        Returns:
+            float: The return value.
+
+        """
+        c = a*b
+        return c
+
+    def div(self, a, b):
+        """Return quotient of two given numbers.
+
+        Given two numbers float type a, b
+        returns float type a/b.
+
+        Arg:
+            a (float): The dividend of dvision function.
+            b (float): The divisor of dvision function.
+
+        Return:
+            float: The quotient fo two given numbers.
+        """
+        return a / b
--- test_calculator.py
+++ test_calculator.py
@@ -35,4 +35,18 @@ class TestCalculator():
 
     def test_div(self):
         """Put your test cases for div that starts with assert."""
-        pass
+        calc = Calculator()
+        result = calc.div(4.2, 2.1)
+        assert result == 2
+
+        result = calc.div(5, 2.)
+        assert result == 2.5
+
+        result = calc.div(100, 20.)
+        assert result == 5
+
+        result = calc.div(5.e10, 10.)
+        assert result == 5.e9
+
+        result = calc.div(1., 1.e5)
+        assert result == 1.e-5

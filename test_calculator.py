9379d99ad4c74d9877244680a036fa9e89cd59c3
subin_yang <ysb8049@naver.com>
2019-01-16 22:12:46 +0900

    Add test case of bitwise not operation of Calculator class
    This is test case of bitwise not operation of Calculator class.

--- test_calculator.py
+++ test_calculator.py
@@ -12,12 +12,29 @@ class TestCalculator():
     """
 
     def test_add(self):
-        """Put your test cases for add that starts with assert."""
-        pass
+        """Test functionality of add method."""
+        x = 1.0
+        y = 1.0
+        calc = Calculator()
+        assert calc.add(x, y) == 2.0
 
     def test_sub(self):
-        """Put your test cases for sub that starts with assert."""
-        pass
+        """Check 3 test cases which varies signs of input for subtraction."""
+        cal = Calculator()
+        assert cal.sub(10.0, 7.3) == 2.7
+        assert cal.sub(6.3, 7.3) == -1.0
+        assert cal.sub(2.0, 3.1) == -1.1
+
+        """Check 3 test cases which varies signs of input for subtraction."""
+        cal = Calculator()
+        result = cal.sub(10.0, 7.3)
+        assert result == 2.7
+
+        result = cal.sub(-6.3, 7.3)
+        assert result == -13.6
+
+        result = cal.sub(-2.0, -3.1)
+        assert result == 1.1
 
     def test_mul(self):
         """Put your test cases for mul that starts with assert."""
@@ -40,6 +57,24 @@ class TestCalculator():
 
         result = calc.div(1., 1.e5)
         assert result == 1.e-5
+
+    def test_bitwise_or(self):
+        """Check 3 test cases which varies length of input for bitwise_or."""
+        calc = Calculator()
+        result = calc.bitwise_or(0b010000, 0b100000)
+        assert result == 0b110000
+
+        result = calc.bitwise_or(0b00011, 0b11000)
+        assert result == 0b11011
+
+        result = calc.bitwise_or(0b0010, 0b1000)
+        assert result == 0b1010
+
+    def test_xor(self):
+        """Test case for calculator's bitwise xor calculations."""
+        calc = Calculator()
+        result = calc.bitwise_xor(60, 13)
+        assert result == 49
 
     def test_bitwise_not(self):
         """Test cases for bitwise_not function in Calculator class.
